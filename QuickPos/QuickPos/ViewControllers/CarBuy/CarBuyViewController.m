9cd4c66d6ae6060ec080d1d695f17d549a8a13b9
tianNanYiHao <851085835@qq.com>
2016-12-07 16:04:43 +0800

    123

---
+++ QuickPos/QuickPos/ViewControllers/CarBuy/CarBuyViewController.m
@@ -0,0 +1,48 @@
+//
+//  CarBuyViewController.m
+//  QuickPos
+//
+//  Created by Lff on 16/12/7.
+//  Copyright © 2016年 张倡榕. All rights reserved.
+//
+
+#import "CarBuyViewController.h"
+
+@interface CarBuyViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableShowView;
+
+@end
+
+@implementation CarBuyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"购物车";
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.barTintColor = [Common hexStringToColor:@"#068bf4"];//导航栏颜色
+    self.navigationController.navigationBar.tintColor = [Common hexStringToColor:@"#ffffff"];//返回键颜色
+    self.navigationController.navigationBar.contentMode = UIViewContentModeScaleAspectFit;
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
